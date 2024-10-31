fd8e9503497d38e534e58659f0b61cf4fb5fe4bd
Your Name <infilol123@gmail.com>
2024-10-31 23:36:53 +0700

    feat: remove try catch in allocations data

--- src/app/actions/alokasi.action.ts
+++ src/app/actions/alokasi.action.ts
@@ -5,27 +5,19 @@ import { Allocation } from "@/lib/types";
 import type { MonthlyAllocations } from "@prisma/client";
 
 export async function getAllokasiAll() {
-  try {
-    const data = await prisma.allocations.findMany({
-      where: {
-        AND: {
-          status: {
-            in: ["Pending", "Approved"],
-          },
+  const data = await prisma.allocations.findMany({
+    where: {
+      AND: {
+        status: {
+          in: ["Pending", "Approved"],
         },
       },
-    });
-    return data as Allocation[];
-  } catch (error) {
-    throw error;
-  }
+    },
+  });
+  return data as Allocation[];
 }
 
 export async function getMonthlyAllocation() {
-  try {
-    const data = await prisma.monthlyAllocations.findMany();
-    return data as MonthlyAllocations[];
-  } catch (error) {
-    throw error;
-  }
+  const data = await prisma.monthlyAllocations.findMany();
+  return data as MonthlyAllocations[];
 }
