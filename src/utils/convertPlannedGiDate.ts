8406b5c790c665faa5ee63a9e570272dca92e826
Your Name <infilol123@gmail.com>
2024-12-13 12:33:11 +0700

    feat: changing format date when upload allocation harian

--- src/utils/convertPlannedGiDate.ts
+++ src/utils/convertPlannedGiDate.ts
@@ -1,8 +1,18 @@
 export function convertStringToDate(dateString: string): Date {
-  const day = parseInt(dateString.slice(0, 2), 10);
-  const month = parseInt(dateString.slice(2, 4), 10) - 1;
-  const year = parseInt(dateString.slice(4, 8), 10);
-
-  // Menggunakan Date.UTC untuk mencegah perubahan waktu oleh zona waktu lokal
+  let day: number, month: number, year: number;
+  if (dateString.length === 7) {
+    // Format DMMYYYY (contoh: 2122024 -> 2-12-2024)
+    day = parseInt(dateString.slice(0, 1), 10); // 1 digit untuk hari
+    month = parseInt(dateString.slice(1, 3), 10) - 1; // 2 digit untuk bulan
+    year = parseInt(dateString.slice(3), 10); // 4 digit untuk tahun
+  } else if (dateString.length === 8) {
+    // Format DDMMYYYY (contoh: 02122024 -> 02-12-2024)
+    day = parseInt(dateString.slice(0, 2), 10); // 2 digit untuk hari
+    month = parseInt(dateString.slice(2, 4), 10) - 1; // 2 digit untuk bulan
+    year = parseInt(dateString.slice(4), 10); // 4 digit untuk tahun
+  } else {
+    console.error("Format tanggal tidak didukung");
+    return new Date(Date.UTC(1970, 0, 1));
+  }
   return new Date(Date.UTC(year, month, day));
 }
