8c85e688749fb151ee995462a4c5f3ba684fe242
Your Name <infilol123@gmail.com>
2025-02-27 14:34:10 +0700

    add: penyaluran-elpiji page and also fix api penyaluran-elpiji

--- src/app/api/penyaluran-elpiji/route.ts
+++ src/app/api/penyaluran-elpiji/route.ts
@@ -22,17 +22,26 @@ export async function POST(req: NextRequest) {
       };
     }
 
-    if (range?.from && range?.to) {
-      const start = new Date(range.from);
-      const end = new Date(range.to);
+    if (range?.from || range?.to) {
+      let start, end;
 
-      // Validasi jika format tanggalnya benar
+      if (range?.from && range?.to) {
+        start = new Date(range.from);
+        end = new Date(range.to);
+      } else {
+        start = new Date(range.from || range.to);
+        end = new Date(range.from || range.to);
+      }
+
       if (isNaN(start.getTime()) || isNaN(end.getTime())) {
         return NextResponse.json(
           { message: "Invalid date format" },
           { status: 400 }
         );
       }
+
+      start.setHours(0, 0, 0, 0);
+      end.setHours(23, 59, 59, 999);
 
       whereConditions.giDate = {
         gte: new Date(start.setHours(0, 0, 0, 0)), // Set waktu awal hari
@@ -42,6 +51,32 @@ export async function POST(req: NextRequest) {
 
     const skip = (page - 1) * pageSize;
     const take = pageSize;
+
+    const totalQty = await prisma.lpgDistributions.aggregate({
+      where:
+        Object.keys(whereConditions).length > 0 ? whereConditions : undefined,
+      _sum: {
+        allocatedQty: true,
+      },
+    });
+
+    // Query jumlah unique agentName langsung di database
+    const totalAgen = await prisma.lpgDistributions.groupBy({
+      by: ["agentName"],
+      where:
+        Object.keys(whereConditions).length > 0 ? whereConditions : undefined,
+    });
+
+    const totalAgenCount = totalAgen.length;
+
+    // Query total alokasi harian (jumlah record)
+    const totalDistribusi = await prisma.lpgDistributions.count({
+      where:
+        Object.keys(whereConditions).length > 0 ? whereConditions : undefined,
+    });
+
+    // Hitung total berat
+    const totalBeratQty = (totalQty._sum.allocatedQty || 0) * 3;
 
     // Query total count untuk pagination
     const totalCount = await prisma.lpgDistributions.count({
@@ -54,7 +89,7 @@ export async function POST(req: NextRequest) {
         Object.keys(whereConditions).length > 0 ? whereConditions : undefined, // Jika tidak ada filter, where akan diabaikan
       skip,
       take,
-      orderBy: { bpeNumber: "asc" },
+      orderBy: { bpeNumber: "desc" },
       select: {
         id: true,
         bpeNumber: true,
@@ -72,18 +107,27 @@ export async function POST(req: NextRequest) {
       },
     });
 
-    return NextResponse.json({
-      message: filteredData.length
-        ? "Data fetched successfully"
-        : "No data found",
-      data: filteredData,
-      pagination: {
-        total: totalCount,
-        page,
-        pageSize,
-        totalPages: Math.ceil(totalCount / pageSize),
+    return NextResponse.json(
+      {
+        message: filteredData.length
+          ? "Data fetched successfully"
+          : "No data found",
+        data: filteredData,
+        cardInfo: {
+          totalQty: totalQty._sum.allocatedQty || 0,
+          totalBeratQty,
+          totalAgenCount,
+          totalDistribusi,
+        },
+        pagination: {
+          total: totalCount,
+          page,
+          pageSize,
+          totalPages: Math.ceil(totalCount / pageSize),
+        },
       },
-    });
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json(
