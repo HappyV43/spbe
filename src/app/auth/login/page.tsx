78b2620597e4d2a343e6daff1024e1ffb41deb8a
Your Name <infilol123@gmail.com>
2025-02-27 14:33:24 +0700

    add: adding tablist in auth/login when there's no user in db

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -10,22 +10,22 @@ const Auth = async () => {
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
       <Tabs defaultValue="login" className="w-auto">
-        {/* <TabsList>
+        <TabsList>
           {hasUsers && (
             <div>
               <TabsTrigger value="login">Login</TabsTrigger>
               <TabsTrigger value="register">Register</TabsTrigger>
             </div>
           )}
-        </TabsList> */}
+        </TabsList>
         <TabsContent value="login">
           <LoginForm />
         </TabsContent>
-        {/* {hasUsers && (
+        {hasUsers && (
           <TabsContent value="register">
             <SignUpForm />
           </TabsContent>
-        )} */}
+        )}
       </Tabs>
     </div>
   );
