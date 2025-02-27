dd3dbc7e6884213992fce2ec38b0c61c068a67bc
Ardine <ardinejivensen@gmail.com>
2025-02-27 14:53:33 +0700

    add a constant folder

--- src/components/Sidebar/AppSidebar.tsx
+++ src/components/Sidebar/AppSidebar.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import type * as React from "react"
+import type * as React from "react";
 import {
   AudioWaveform,
   BookOpen,
@@ -15,103 +15,54 @@ import {
   PieChart,
   Settings2,
   SquareTerminal,
-} from "lucide-react"
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from "../ui/sidebar"
-import Image from "next/image"
-import { Button } from "../ui/button"
-import { toast } from "@/hooks/use-toast"
-import { logOut } from "@/app/actions/auth.actions"
-import { LogoutSidebar } from "./LogoutSidebar"
-import { MainSidebar } from "./MainSidebar"
-import { SummarySidebar } from "./SummarySidebar"
-import { MasterSidebar } from "./MasterSidebar"
-
-const data = {
-  // user: {
-  //   name: "shadcn",
-  //   email: "m@example.com",
-  //   avatar: "/avatars/shadcn.jpg",
-  // },
-  summary: [
-    {
-      name: "Summary",
-      url: "/summary",
-      icon: ChartArea,
-    },
-  ],
-  dashboard: [
-    {
-      title: "Dashboard",
-      url: "/dashboard/penyaluran-elpiji",
-      icon: House,
-      isActive: true,
-      items: [
-        {
-          title: "Alokasi Harian",
-          url: "/dashboard/alokasi-harian",
-        },
-        {
-          title: "Penyaluran Elpiji",
-          url: "/dashboard/penyaluran-elpiji",
-        },
-        {
-          title: "Alokasi Bulanan",
-          url: "/dashboard/alokasi-bulanan",
-        },
-      ],
-    },
-  ],
-  masterData: [
-    {
-      title: "Master Data",
-      url: "/master-data/agents",
-      icon: Database,
-      // isActive: true,
-      items: [
-        {
-          title: "Agen",
-          url: "/master-data/agents",
-        },
-        {
-          title: "Perusahaan",
-          url: "/master-data/companies",
-        },
-      ],
-    },
-  ],
-}
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "../ui/sidebar";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { toast } from "@/hooks/use-toast";
+import { logOut } from "@/app/actions/auth.actions";
+import { LogoutSidebar } from "./LogoutSidebar";
+import { MainSidebar } from "./MainSidebar";
+import { SummarySidebar } from "./SummarySidebar";
+import { MasterSidebar } from "./MasterSidebar";
+import { sidebarItems } from "@/constants/sidebarItems.constant";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-    const handleClick = async () => {
-        const result = await logOut();
-        if (result?.error) {
-          toast({
-            title: "Gagal",
-            description: result.error,
-            variant: "destructive",
-          });
-        } else {
-          toast({
-            title: "Logout berhasil",
-          });
-        }
-      };
-    
-    return (
+  const handleClick = async () => {
+    const result = await logOut();
+    if (result?.error) {
+      toast({
+        title: "Gagal",
+        description: result.error,
+        variant: "destructive",
+      });
+    } else {
+      toast({
+        title: "Logout berhasil",
+      });
+    }
+  };
+
+  return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader className="flex justify-center items-center">
         <Image src="/icon.svg" width={125} height={125} alt="Icon PKMU" />
       </SidebarHeader>
       <SidebarContent>
-        <SummarySidebar items={data.summary}/>
-        <MainSidebar items={data.dashboard}/>
-        <MasterSidebar items={data.masterData}/>
+        <SummarySidebar items={sidebarItems.summary} />
+        <MainSidebar items={sidebarItems.dashboard} />
+        <MasterSidebar items={sidebarItems.masterData} />
       </SidebarContent>
       <SidebarFooter>
-        <LogoutSidebar onLogout={handleClick}/>
+        <LogoutSidebar onLogout={handleClick} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
-
